ba1db367d6ce5888197e21b90509e94495816c47
Maranes Alejandro <alejandro.maranes@asendia.com>
2022-02-11 21:42:57 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect  } from 'react';
+import { BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import useWindowSize from './hooks/useWindowSize';
+import Sidebar from './component/sidebar/sidebar';
+import MobileNavigation from './component/sidebar/mobile-navigation';
+import Footer from './component/footer/footer';
+import Home from './pages/home';
+import Search from './pages/search';
+import Library from './pages/library';
+import PlaylistPage from './pages/playlist';
+
+import CONST from './constants/index';
+import { PLAYLIST } from './data/index';
+import styles from './style/App.module.css';
 
 function App() {
+  const size = useWindowSize();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+        <div className={styles.layout}>
+          {size.width > CONST.MOBILE_SIZE 
+            ? <Sidebar /> 
+            : <MobileNavigation />
+          }
+          <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/search">
+                <Search />
+            </Route>
+            <Route path="/library">
+                <Library />
+            </Route>
+            <Route exact path="/playlist/:path">
+                <PlaylistPage />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./reducers/index";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './style/index.css';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
